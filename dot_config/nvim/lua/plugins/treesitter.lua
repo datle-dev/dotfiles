acf25fb81f9cd0adc810af150c5f0300a2f0da43
Dat Le <dle.dev@outlook.com>
2025-06-15 05:25:08 -0400

    update

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,6 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     branch = "main",
-    lazy = false,
     build = ":TSUpdate",
     opts = {
       ensure_installed = {
@@ -16,7 +15,18 @@ return {
         "toml",
         "yaml",
       },
-      highlight = { enable = true },
+      auto_install = false,
+      highlight = {
+        enable = true,
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.looop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
+      additional_vim_regex_highlighting = false,
     },
     config = function(_, opts)
       require("nvim-treesitter").setup(opts)
